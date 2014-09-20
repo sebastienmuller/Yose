2cfc8554fc140a1c967d5e492ffe1e5ff06c0275
Sébastien Muller <seb.muller@free.fr>
2014-09-20 23:12:33 +0200

    Ping

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using YoseApp.Controllers;
+using System.Web.Mvc;
+
+namespace Tests
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void Index()
+        {
+            var pingCtrl = new PingController();
+
+            var result = pingCtrl.Index();
+
+            //assert
+            Assert.IsInstanceOf<JsonResult>(result);
+            var data = ((JsonResult)result).Data;
+            var type = data.GetType();
+            var alivePropertyInfo = type.GetProperty("alive");
+            var actual = alivePropertyInfo.GetValue(data, null);
+
+            Assert.AreEqual(true, actual);
+        }
+    }
+}
---
+++ YoseApp/Controllers/PingController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YoseApp.Controllers
+{
+    public class PingController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Json(new { alive = true });
+        }
+	}
+}
